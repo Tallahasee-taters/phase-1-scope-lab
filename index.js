af4e2a20eaf569be605adce3d0ecb2308fb9a3fd
Tate <thandley1234@gmail.com>
2022-11-26 14:48:15 -0700

    commit tree bd1384697b8c9abcf1843b785b8deb576a3bb0a6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    window.customerName = 'BOB'
+}
+function setBestCustomer(){
+    window.bestCustomer = 'not bob' 
+}
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    throw ('Assignment to constant variable.')
+}
